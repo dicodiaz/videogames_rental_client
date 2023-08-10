58ea34b4757b2be91c5f8c28aa8411aa32553d17
Dico Diaz <dicodiaz@gmail.com>
2023-08-10 15:12:19 -0500

    Update mock data

--- src/__mocks__/mockReservations.js
+++ src/__mocks__/mockReservations.js
@@ -1,43 +1,43 @@
 export const mockReservation = {
-  id: 26,
-  user_id: 7,
-  videogame_id: 68,
-  days: 11,
-  total_price: '220.0',
-  created_at: '2023-07-20T17:53:13.512Z',
-  updated_at: '2023-07-20T17:53:13.512Z',
-  videogame: {
-    id: 68,
-    name: 'Street Fighter',
-    photo: 'https://mms.businesswire.com/media/20230122005013/en/1692292/5/p1-1.jpg',
-    description:
-      'Street Fighter is a 1987 arcade game developed by Capcom. It is the first competitive fighting game produced by the company and the inaugural game in the Street Fighter series.',
-    price_per_day: '20.0',
-    created_at: '2023-07-20T17:37:47.367Z',
-    updated_at: '2023-07-20T17:37:47.367Z',
-  },
-};
-
-const mockReservations = [
-  mockReservation,
-  {
-    id: 19,
-    user_id: 7,
-    videogame_id: 62,
+  id: '17',
+  type: 'reservation',
+  attributes: {
+    id: 17,
+    user_id: 10,
     days: 2,
     total_price: '10.0',
-    created_at: '2023-07-20T17:37:47.766Z',
-    updated_at: '2023-07-20T17:37:47.766Z',
+    created_at: '2023-08-10T20:01:19.767Z',
     videogame: {
-      id: 62,
+      id: 31,
       name: 'Tetris',
       photo:
         'https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Tetris_NES_cover_art.jpg/220px-Tetris_NES_cover_art.jpg',
       description:
         'Tetris is a puzzle video game created in 1984 by Alexey Pajitnov, a Soviet software engineer.',
       price_per_day: '5.0',
-      created_at: '2023-07-20T17:37:47.068Z',
-      updated_at: '2023-07-20T17:37:47.068Z',
+    },
+  },
+};
+
+const mockReservations = [
+  mockReservation,
+  {
+    id: '16',
+    type: 'reservation',
+    attributes: {
+      id: 16,
+      user_id: 10,
+      days: 3,
+      total_price: '30.0',
+      created_at: '2023-08-10T20:01:19.759Z',
+      videogame: {
+        id: 30,
+        name: 'Super Mario Bros',
+        photo: 'https://upload.wikimedia.org/wikipedia/en/0/03/Super_Mario_Bros._box.png',
+        description:
+          'Super Mario Bros is a platform game developed and published by Nintendo. The successor to the 1983 arcade game Mario Bros. and the first game in the Super Mario series, it was first released in 1985 for the Famicom in Japan.',
+        price_per_day: '10.0',
+      },
     },
   },
 ];
